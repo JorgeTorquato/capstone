4ece367a8ee606ccfe55e06ce0f8cc99e279bfc0
JorgeTorquato <ellendel@gmail.com>
2022-08-15 18:28:46 -0400

    added authentication page and sign in form

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,16 +1,19 @@
 import './button.styles.scss';
-const buttonTypeClasses = {
-  google: 'google-sing-in',
+
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sign-in',
   inverted: 'inverted',
 };
+
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
     <button
-      className={`button-container ${buttonTypeClasses[buttonType]}`}
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
       {...otherProps}
     >
       {children}
     </button>
   );
 };
+
 export default Button;
